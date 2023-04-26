1aa14cc67921e3b34cd40cd574fc0f2d8cb638e1
Railson Ferreira <railson.info@gmail.com>
2023-04-27 00:41:23 +0100

    chore: Update lint shell script

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
-dart analyze;
-dart run dart_code_metrics:metrics analyze lib test;
-dart run dart_code_metrics:metrics check-unused-files lib test;
-dart run dart_code_metrics:metrics check-unused-l10n lib test;
+dart analyze
+dart run dart_code_metrics:metrics analyze lib test --fatal-warnings --fatal-performance --fatal-style
+dart run dart_code_metrics:metrics check-unused-files lib test --fatal-unused
+dart run dart_code_metrics:metrics check-unused-l10n lib test --fatal-unused
